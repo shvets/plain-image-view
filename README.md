36fb07cd0bfb592c19db2d4ace839f84b59ea733
Alexander Shvets <33634796+shvets-semandex@users.noreply.github.com>
2024-07-09 21:57:32 -0400

    clean up project

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ View for representing image
 ## Publishing
 
 ```bash
-git tag 1.0.1
+git tag 1.0.2
 git push --tags
 ```
--- Sources/plain-image-view/ImageView.swift
+++ Sources/plain-image-view/ImageView.swift
@@ -1,34 +1,58 @@
 import SwiftUI
 
 public struct ImageView: View {
-  var image: UIImage
-  var customizeImage: (Image) -> any View
+  var image: UIImage?
 
-  public init(image: UIImage, customizeImage: @escaping (Image) -> any View) {
+  public init(image: UIImage? = nil) {
     self.image = image
-    self.customizeImage = customizeImage
   }
 
   public var body: some View {
     content()
   }
 
-  #if !os(tvOS)
+#if !os(tvOS)
   @ViewBuilder
   func content() -> some View {
-    AnyView(customizeImage(Image(uiImage: image)))
+    if let image = image {
+      Image(uiImage: image)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .scaledToFit()
+        .cornerRadius(5)
+    }
+    else {
+      Image(uiImage: UIImage(systemName: "photo")!)
+        .resizable()
+        .scaledToFill()
+        .cornerRadius(10)
+        .padding(5)
+        .redacted(reason: .placeholder)
+    }
   }
-  #endif
+#endif
 
-  #if os(tvOS)
+#if os(tvOS)
   @ViewBuilder
   func content() -> some View {
     Rectangle()
       .aspectRatio(1, contentMode: .fill)
       .overlay {
-        AnyView(customizeImage(Image(uiImage: image)))
+        if let image = image {
+          Image(uiImage: image)
+            .resizable()
+            .scaledToFill()
+        }
+        else {
+          Image(uiImage: UIImage(systemName: "photo")!)
+            .resizable()
+            .scaledToFill()
+            .cornerRadius(10)
+            .padding(5)
+            .redacted(reason: .placeholder)
+        }
       }
       .cornerRadius(5)
   }
-  #endif
+#endif
 }
